fb6b99ed9b5a839991b883b370499acf7046fdc2
DanyTPG <50212295+DanyTPG@users.noreply.github.com>
2021-08-04 18:37:40 +0430

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,47 +1,35 @@
 #!/bin/sh
-apk update && apk add --no-cache --virtual .build-deps ca-certificates curl unzip 
-# Download and install V2Ray
 
-mkdir /tmp/v2ray
-curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
-install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+#Config xray
 
-# Remove temporary directory
-rm -rf /tmp/v2ray
-
-# V2Ray new configuration
-install -d /usr/local/etc/v2ray
-cat << EOF > /usr/local/etc/v2ray/config.json
+rm -rf /etc/xray/config.json
+cat << EOF > /etc/xray/config.json
 {
-    "inbounds": [
-        {
-            "port": $PORT,
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "$ID", 
-                        "flow": "xtls-rprx-direct",
-                        "level": 0,
-                        "email": "love@v2fly.org"
-                    }
-                ],
-                "decryption": "none"
-            },
-            "streamSettings": {
-                "network": "ws"
-            }
-        }
-    ],
-    "outbounds": [
-        {
-            "protocol": "freedom"
-        }
-    ]
+  "inbounds": [
+    {
+      "port": $PORT,
+      "protocol": "vless",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "$UUID"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
 }
 EOF
 
-# Run V2Ray
-/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+#run xray
+
+xray -c /etc/xray/config.json
