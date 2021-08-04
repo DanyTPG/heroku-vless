a7427c667dc740245103365b617e5114151476ef
DanyTPG <50212295+DanyTPG@users.noreply.github.com>
2021-08-04 18:23:20 +0430

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-
+apk update && apk add --no-cache --virtual .build-deps ca-certificates curl unzip && apk del .build-deps
 # Download and install V2Ray
+
 mkdir /tmp/v2ray
 curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
