e14964682242a8f292d49b3b8c515236b87c4dcd
DanyTPG <50212295+DanyTPG@users.noreply.github.com>
2023-04-09 11:26:14 +0330

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,10 +2,10 @@
 
 # Download and install V2Ray
 mkdir /tmp/v2ray
-curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
-install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+install -m 755 /tmp/v2ray/xray /usr/local/bin/xray
+
 
 # Remove temporary directory
 rm -rf /tmp/v2ray
@@ -43,4 +43,4 @@ cat << EOF > /usr/local/etc/v2ray/config.json
 EOF
 
 # Run V2Ray
-/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+/usr/local/bin/xray run -c /usr/local/etc/v2ray/config.json
